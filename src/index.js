652832eba0fe65a9ad28b7c59a79a0c13b75c099
Daksh Yadav <dy.chander555@gmail.com>
2024-11-17 01:04:10 +0530

    Custom api response

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ dotenv.config({
 //2nd approach====>
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err)
+})
 
 
 
